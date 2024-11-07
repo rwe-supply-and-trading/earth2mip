b10bc1744525949c8d029290ae7f7531372f03f3
Diana Gergel <diana.gergel@rwe.com>
2024-11-07 15:20:48 -0800

    update path assumption

--- earth2mip/initial_conditions/ifs.py
+++ earth2mip/initial_conditions/ifs.py
@@ -58,7 +58,7 @@ def _get_channel(c: str, **kwargs) -> xarray.DataArray:
 
 def get(time: datetime.datetime, channels: List[str], ensemble_member: int, 
         root_path: str): -> xarray.DataArray:
-    path = root_path + _get_filename(time, "0h")
+    path = os.path.join(root_path, _get_filename(time, "0h"))
     # open as list of Datasets given structure of grib 
     dataset_0h = cfgrib.open_datasets(path)
 

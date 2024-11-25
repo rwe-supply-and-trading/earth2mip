e4027b11d7391a59aecbb4da7e112fdb70311193
Diana Gergel <diana.gergel@rwe.com>
2024-11-25 12:01:52 -0800

    add more options needed by initial conditions function

--- earth2mip/schema.py
+++ earth2mip/schema.py
@@ -101,6 +101,8 @@ class EnsembleRun(pydantic.BaseModel):
         model_registry (optional): The path to the model registry (alternative to the default, $HOME/.cache/earth2mip/models
 	world_size (optional): Number of accelerators being used for the ensemble inference run
 	model_checkpoint (optional): Checkpoint to use if SFNO HENS weights are being used for inference
+	ic_path (optional): If specified, load initial conditions from this path. Only valid currently for EC IFS. 
+	ensemble_member (optional): If specified, load initial conditions for the ensemble member number provided. Only valid for IFS. 0 indicates the control run. 
 	restart_frequency: if provided save at end and at the specified frequency. 0 = only save at end.
         grf_noise_alpha: tuning parameter of the Gaussian random field, see ensemble_utils.generate_noise_grf for details
         grf_noise_sigma: tuning parameter of the Gaussian random field, see ensemble_utils.generate_noise_grf for details
@@ -129,6 +131,8 @@ class EnsembleRun(pydantic.BaseModel):
     model_registry: Optional[str] = None
     world_size: Optional[str] = None
     model_checkpoint: Optional[str] = None
+    ic_path: Optional[str] = None
+    ensemble_member: Optional[int] = None
     restart_frequency: Optional[int] = None
     grf_noise_alpha: float = 2.0
     grf_noise_sigma: float = 5.0
